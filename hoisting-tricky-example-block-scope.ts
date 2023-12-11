3a8c33061be3f10bace902eb5c1a45207a89129a
Abhishek Kumar <abhishek.km2010@gmail.com>
2023-12-11 16:28:19 +0530

    Update and rename hoisting-tricky-example.ts to hoisting-tricky-example-block-scope.ts

--- hoisting-tricky-example-block-scope.ts
+++ hoisting-tricky-example-block-scope.ts
@@ -1,5 +1,7 @@
 // let and const can't be hoisted
 
+// Example 1: 
+
 // hoisting tricky example:-
 {
   a = 10;
@@ -11,3 +13,20 @@ var a = 100; // 100
 let b = 200; // Error: cannot access b before initiallization
 const c = 300; // Error: cannot access a before initiallization
 console.log(a);
+
+
+// Example 2:
+
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// {
+//   a = 100;
+//   b = 200;
+//   // c = 300; // Error
+//   // console.log(a, b, c);
+// }
+
+// console.log(a, b, c); // 100, 200, 30
