cca01e03fed5f0265215ce9479f99a5e9edde177
Abhishek Kumar <abhishek.km2010@gmail.com>
2023-12-11 14:47:35 +0530

    Create call-apply-bind.ts

---
+++ call-apply-bind.ts
@@ -0,0 +1,20 @@
+import { Gender, Person } from "../common/model";
+
+function greet(this: any, greeting1, greeting2) {
+  return `${greeting1} ${this.firstName} ${this.lastName}, ${greeting2}`;
+}
+
+const person: Person = {
+  firstName: 'John',
+  lastName: 'Doe',
+  gender: Gender.male
+}
+
+const callResult: string = greet.call(person, 'Hello', 'How r u ?');
+console.log(callResult);
+
+const applyResult: string = greet.apply(person, ['Hii', 'How Are you ?']);
+console.log(applyResult);
+
+const bindResult = greet.bind(person, 'Hey', 'What is up ?');
+console.log(bindResult());
