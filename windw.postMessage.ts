ddff33706b02b6d0b104bda4b89f78fc75494907
Abhishek Kumar <abhishek.km2010@gmail.com>
2023-12-12 09:34:47 +0530

    Create windw.postMessage.ts

---
+++ windw.postMessage.ts
@@ -0,0 +1,6 @@
+https://medium.com/@mrajaeim/understanding-window-postmessage-and-window-parent-postmessage-in-javascript-f09d4eac68ba#:~:text=postMessage%20method%20is%20a%20secure,they%20originate%20from%20different%20domains.
+
+// used for cross origin communication between window objects
+//Post message is a method that enables cross-origin communication between Window objects.
+ // (i.e, between a page and a pop-up that it spawned, or between a page and an iframe embedded within it). 
+  //Generally, scripts on different pages are allowed to access each other if and only if the pages follow same-origin policy(i.e, pages share the same protocol, port number, and host).
